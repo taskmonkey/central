a402a730cb448e6b70e756071c4386d83ad06f10
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-08-05 21:54:50 -0700

    Trying another pull for the recalibrated Auth.js file

--- Client/src/Auth/Auth.js
+++ Client/src/Auth/Auth.js
@@ -1,10 +1,13 @@
+// import fs from 'fs';
 import auth0 from 'auth0-js';
+import axios from 'axios';
+import ClientID from '../../config.js'
 
 export default class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
       domain: 'austenwma.auth0.com',
-      clientID: 'lczGQdQdeATDVsDoPJc7zoTGjHTFSl2X',
+      clientID: ClientID.CLIENT_ID,
       redirectUri: 'http://localhost:3000',
       audience: 'https://austenwma.auth0.com/userinfo',
       responseType: 'token id_token',
