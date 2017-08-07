4d144d76a6986c2db418d92825893c06c4122c31
Daniel Ham <danieljham@gmail.com>
2017-08-07 16:58:16 -0700

    catching up after re-base

--- Client/src/Reducers/reducer_tasks.js
+++ Client/src/Reducers/reducer_tasks.js
@@ -1,12 +1,14 @@
 import { FETCH_TASKS } from '../Actions/index';
 
-const INITIAL_STATE = {allTasks: [], task: null};
+const INITIAL_STATE = { allTasks: [], task: null };
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case FETCH_TASKS:
-      return Object.assign({}, state, {allTasks: action.payload.data});
-    default:
-      return state;
+  case FETCH_TASKS:
+    //return state.allTasks.concat([action.payload.data]);
+    // return {...state, allTasks: action.payload.data};
+    return Object.assign({}, state, {allTasks: action.payload.data});
+  default:
+    return state;
   }
 }
