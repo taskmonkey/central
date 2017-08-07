e1c9d6c5bb0f9bbf56da185ee46c9658aff6f977
rsong003 <richsong003@gmail.com>
2017-08-07 14:31:03 -0700

    working on dashboard config

--- Client/src/Components/Containers/Dashboard/Dashboard.jsx
+++ Client/src/Components/Containers/Dashboard/Dashboard.jsx
@@ -3,15 +3,26 @@ import {render} from 'react-dom';
 import {Redirect, Link, withRouter} from 'react-router-dom';
 import PieGraph from './PieChart.jsx'
 import BarGraph from './BarChart.jsx'
+import NavTask from './NavTask.jsx'
 
 class Dashboard extends Component{
   render() {
     return(
-      <div>
-        <h1>Task Monkey Dashboard</h1>
-        <PieGraph />
-        <BarGraph />
-        <Link to="/tasksList"><button>Tasks List</button></Link>
+      <div className = "container">
+        <h1>Task Mon Dashboard</h1>
+        <div className="row">
+          <div className="col-sm-2">
+            <NavTask />
+          </div>
+          <div className ="col-sm-5">
+            <PieGraph />
+          </div>
+          <div className ="col-sm-5">
+            <BarGraph />
+          </div>
+        </div>
+          <Link to="/tasksList"><button>Tasks List</button></Link>
+        
       </div>
     )
   }
