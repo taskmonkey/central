b04b7415b39eba0bed911e5d0236cf01448bb61b
rsong003 <richsong003@gmail.com>
2017-08-06 19:52:23 -0700

    fixed merge conflicts

--- Client/src/Components/Containers/Dashboard/Dashboard.jsx
+++ Client/src/Components/Containers/Dashboard/Dashboard.jsx
@@ -1,13 +1,8 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Redirect, Link, withRouter} from 'react-router-dom';
-<<<<<<< HEAD
 import PieGraph from './PieChart.jsx'
 import BarGraph from './BarChart.jsx'
-=======
-import PieDemo from './PieChart.jsx'
-
->>>>>>> 3ac5ff0791d9de05d4e36aad4e69a754d91e62cd
 
 class Dashboard extends Component{
 
@@ -15,12 +10,8 @@ class Dashboard extends Component{
     return(
       <div>
         <h1>Task Monkey Dashboard</h1>
-<<<<<<< HEAD
         <PieGraph />
         <BarGraph />
-=======
-        <PieDemo />
->>>>>>> 3ac5ff0791d9de05d4e36aad4e69a754d91e62cd
         <Link to="/tasksList"><button>Tasks List</button></Link>
       </div>
     )
