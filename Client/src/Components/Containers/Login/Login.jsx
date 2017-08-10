2e68c09726ca2e99355b74cfd199ee4e2dbde752
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-08-09 18:02:25 -0700

    Added a fade in for login page

--- Client/src/Components/Containers/Login/Login.jsx
+++ Client/src/Components/Containers/Login/Login.jsx
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 import {Redirect,Link, withRouter} from 'react-router-dom'
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 import Auth from '../../../Auth/Auth.js';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Login extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class Login extends Component {
       if (this.state.auth.isAuthenticated()) {
         this.props.history.push('/dashboard')
       }
-    }, 100)
+    }, 200)
   }
 
   checkAuth(e) {
@@ -52,65 +53,70 @@ class Login extends Component {
       //   <button onClick={this.state.auth.logout}>Logout</button>
       //   <Link to="/dashboard"><button>Bypass Authentication to Dashboard</button></Link>
       // </div>
-      <div className="landing">
-        <nav className="navbar navbar-inverse">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <p className="navbar-brand">TaskMon</p>
+      <ReactCSSTransitionGroup transitionName="example"
+      transitionAppear={true}
+      transitionAppearTimeout={1000}
+      transitionEnter={false}
+      transitionLeave={false}>
+        <div className="landing">
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <p className="navbar-brand">TaskMon</p>
+              </div>
+              <ul className="nav navbar-nav">
+                <li><Link to="/dashboard">Bypass Auth to Dashboard</Link></li>
+                <li onClick={this.checkAuth}><a>Check Auth</a></li>
+                <li onClick={(e) => {e.preventDefault(); this.goToDashboard();}}><a>Go To Dashboard</a></li>
+                <li onClick={(e) => {e.preventDefault(); this.state.auth.logout(); alert('logged out')}}><a>Logout</a></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li onClick={this.state.auth.login}><a><span className="glyphicon glyphicon-user"></span> Sign Up/Login</a></li>
+              </ul>
             </div>
-            <ul className="nav navbar-nav">
-              <li><Link to="/dashboard">Bypass Auth to Dashboard</Link></li>
-              <li onClick={this.checkAuth}><a>Check Auth</a></li>
-              <li onClick={(e) => {e.preventDefault(); this.goToDashboard();}}><a>Go To Dashboard</a></li>
-              <li onClick={(e) => {e.preventDefault(); this.state.auth.logout(); alert('logged out')}}><a>Logout</a></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li onClick={this.state.auth.login}><a><span className="glyphicon glyphicon-user"></span> Sign Up/Login</a></li>
-            </ul>
+          </nav>
+          <div className="jumbotron">
+            <h1>YOUR LAST TASK MANAGER</h1>
+            {/* <img src="http://under30ceo.com/wp-content/uploads/2013/06/Small-Business-Task-Management.jpg" /> */}
+             {/* <Sparklines data={[5, 10, 5, 20, 50, 30, 70]}>
+              <SparklinesLine />
+              <SparklinesReferenceLine type="mean" />
+            </Sparklines>     */}
           </div>
-        </nav>
-        <div className="jumbotron">
-          <h1>YOUR LAST TASK MANAGER</h1>
-          {/* <img src="http://under30ceo.com/wp-content/uploads/2013/06/Small-Business-Task-Management.jpg" /> */}
-           {/* <Sparklines data={[5, 10, 5, 20, 50, 30, 70]}>
-            <SparklinesLine />
-            <SparklinesReferenceLine type="mean" />
-          </Sparklines>     */}
-        </div>
-        <div className="landing-bottom">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-5">
-                <h4 className="landing-header">Real time data</h4>
-                <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
-                  Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
-                  ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+          <div className="landing-bottom">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-5">
+                  <h4 className="landing-header">Real time data</h4>
+                  <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
+                    Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
+                    ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+                </div>
+                <div className="col-sm-5 col-sm-offset-2">
+                  <h4 className="landing-header">interactive</h4>
+                  <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
+                    Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
+                    ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+                </div>
               </div>
-              <div className="col-sm-5 col-sm-offset-2">
-                <h4 className="landing-header">interactive</h4>
-                <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
-                  Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
-                  ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-5">
-                <h4 className="landing-header">multi-layered</h4>
-                <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
-                  Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
-                  ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
-              </div>
-              <div className="col-sm-5 col-sm-offset-2">
-                <h4 className="landing-header">flexible</h4>
-                <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
-                  Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
-                  ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+              <div className="row">
+                <div className="col-sm-5">
+                  <h4 className="landing-header">multi-layered</h4>
+                  <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
+                    Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
+                    ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+                </div>
+                <div className="col-sm-5 col-sm-offset-2">
+                  <h4 className="landing-header">flexible</h4>
+                  <p className="landing-blurbs">Lorem ipsum dolor sit amet, quo eu ceteros persecuti, unum omnis gubergren his id.
+                    Ei sit graeco facilis, cum ei graece mandamus. Sea affert laboramus voluptatibus no,
+                    ad qui duis noster similique. Ad dolorem noluisse torquatos vim.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
+      </ReactCSSTransitionGroup>
     )
   }
 }
